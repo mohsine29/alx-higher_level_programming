0ea02dfa875fe365a5df94c44ca56e24f30bfdad
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2024-01-04 11:51:10 -0800

    task6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,8 +2,8 @@
 
 if __name__ == "__main__":
     """Handles basic operations"""
-    import sys
     from calculator_1 import add, sub, mul, div
+    import sys
 
     if (len(sys.argv) - 1) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
