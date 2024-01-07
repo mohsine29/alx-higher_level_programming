f1e0e92807a1d586699b783614b40feaa65be247
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2024-01-07 05:16:00 -0800

    task5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    char_list = list(my_string)
+    for x in char_list:
+        if x == 'c' or x == 'C':
+            char_list.remove(x)
+            return "".join(char_list)
