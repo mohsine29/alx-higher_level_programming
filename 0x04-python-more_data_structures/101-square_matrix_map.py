5845d9fc178f66fddb9e0818c83c11bfc0793449
mohsine29 <hourmatallahmohsine@gmail.com>
2024-01-10 12:39:00 -0800

    task14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix:
-        return [list(map(lambda j: j**2, i)) for i in matrix]
+        return (list(map(lambda r: list(map(lambda x: x**2, r)), matrix)))
