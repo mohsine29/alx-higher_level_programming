c554315155d18d465cfc376907bcadbed7475131
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2024-01-22 12:23:28 -0800

    task8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 from sys import stderr
 def safe_function(fct, *args):
     try:
-        return (fct(*args))
-    except (ZeroDivisionError, IndexError, TypeError, ValueError) as e:
+        return fct(*args)
+    except Exception as e:
         stderr.write("Exception: {}\n".format(e))
         return None
