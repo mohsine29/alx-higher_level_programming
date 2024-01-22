e21675bbfd51237a22c09c7fbcafa62721b882c0
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2024-01-22 12:35:10 -0800

    task9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,6 +7,6 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             res += a ** b / i
         except Exception:
-            re = b + a
+            res = b + a
             break
     return res
