f1d32a6532fcd447b7db876fc732e072f3386ef5
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2024-01-27 08:01:54 -0800

    task5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,72 +1,52 @@
-
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
-    """unittest class for max_integer"""
+
+    """run test with python3 -m unittest -v tests.6-max_integer_test"""
+
     def test_module_docstring(self):
-        """Tests for module docsting"""
-        m = __import__('6-max_integer').__doc__
-        self.assertTrue(len(m) > 1)
+        moduleDoc = __import__('6-max_integer').__doc__
+        self.assertTrue(len(moduleDoc) > 1)
 
     def test_function_docstring(self):
-        """Tests for funstion docstring"""
-        f = max_integer.__doc__
-        self.assertTrue(len(f) > 1)
+        functionDoc = __import__('6-max_integer').max_integer.__doc__
+        self.assertTrue(len(functionDoc) > 1)
 
-    def test_empty_list(self):
-        """Tests for empty list []"""
-        e = []
-        self.assertIsNone(max_integer(e))
+    def test_signed_ints_and_floats(self):
+        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 3, -4]), 3)
+        self.assertEqual(max_integer([-1.5, -2.5]), -1.5)
+        self.assertEqual(max_integer([10, -10, 10]), 10)
+        self.assertEqual(max_integer([{1, 9}, {2}, {3}]), {1, 9})
 
-    def test_no_args(self):
-        """Tests for no arguments passed to func"""
-        self.assertIsNone(max_integer())
+    def test_list_of_strings(self):
+        self.assertEqual(max_integer("6789"), '9')
+        self.assertEqual(max_integer("abcxyz"), 'z')
+        self.assertEqual(max_integer(['a', 'b', 'c', 'x', 'y', 'z']), 'z')
+        self.assertEqual(max_integer(["abc", 'x']), 'x')
 
-    def test_one_element(self):
-        """Tests for only one number in the list"""
-        o = [1]
-        self.assertEqual(max_integer(o), 1)
+    def test_lists(self):
+        self.assertEqual(max_integer([[1, 2], [1, 3]]), [1, 3])
 
-    def test_positive_end(self):
-        """Tests for all positive with max at end"""
-        e = [2, 10, 8, 36, 14, 50]
-        self.assertEqual(max_integer(e), 50)
-
-    def test_positive_middle(self):
-        """Tests for all positive with max in middle"""
-        m = [2, 10, 8, 360, 14, 50]
-        self.assertEqual(max_integer(m), 360)
-
-    def test_positive_beginning(self):
-        """Tests for all positive with max at beginning"""
-        b = [200, 10, 8, 36, 14, 50]
-        self.assertEqual(max_integer(b), 200)
-
-    def test_one_negative(self):
-        """Tests for list with one negative number"""
-        on = [200, 10, 8, -36, 14, 50]
-        self.assertEqual(max_integer(on), 200)
-
-    def test_all_negative(self):
-        """Tests for list with all negative numbers"""
-        n = [-6, -50, -75, -1, -100]
-        self.assertEqual(max_integer(n), -1)
-
-    def test_none(self):
-        """Tests for passing none as argument"""
+    def test_other_sequences(self):
         with self.assertRaises(TypeError):
-            max_integer(None)
-
-    def test_non_int_arg(self):
-        """Tests for a non-int type in list"""
-        string = [1, 2, "Hello", 4, 5]
+            max_integer({1, 2}, {3, 4, 5})
         with self.assertRaises(TypeError):
-            max_integer(string)
+            max_integer({1, 2, 3, 4, 5})
+        with self.assertRaises(TypeError):
+            max_integer([-10, 0.5, "str", {1, 2}])
+        with self.assertRaises(TypeError):
+            max_integer([None, True])
+
+    def test_None(self):
+        self.assertIsNone(max_integer([]), None)
+        self.assertIsNone(max_integer(), None)
+        self.assertIsNone(max_integer([None]), None)
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main())
