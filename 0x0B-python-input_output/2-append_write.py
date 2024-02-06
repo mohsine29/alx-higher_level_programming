07d8c226535155f0fbcd8fe48da121e8739157d7
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2024-02-06 08:04:22 -0800

    task2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains the function "append_write"
+"""
+
+
+def append_write(filename="", text=""):
+    """returns the number of characters added"""
+    with open(filename, 'a', encoding='utf=8') as f:
+        return f.write(text)
