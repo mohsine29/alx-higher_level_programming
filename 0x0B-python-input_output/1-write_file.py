bf67e22bb6462a806bef860de21e9e0e28a8c971
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2024-02-06 07:55:35 -0800

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,6 +7,6 @@ Contains the write_file function
 def write_file(filename="", text=""):
     """writes a string to a text file (UTF8)
     and returns the number of characters written."""
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename) as f:
         count = len(f.readlines())
     return count
