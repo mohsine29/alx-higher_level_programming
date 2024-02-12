346e6a97fcca4509b0ff1e65f62751b49c65f86c
mohsine29 <125808498+mohsine29@users.noreply.github.com>
2024-02-12 13:30:58 -0800

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -79,15 +79,14 @@ class Base():
     def load_from_file(cls):
         """Returns list of instances"""
         filenamee = cls.__name__ + ".json"
-        l = []
+        lst = []
         try:
             with open(filenamee, "r") as f:
                 instances = cls.from_json_string(f.read())
             for i, dic in enumerate(instances):
-                l.append(cls.create(**instances[i]))
-        except:
-            pass
-        return l
+                lst.append(cls.create(**instances[i]))
+        except IOError:
+            return lst
 
     @classmethod
     def save_to_file_csv(cls, list_objs):
