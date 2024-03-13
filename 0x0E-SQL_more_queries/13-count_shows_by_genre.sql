047def5798742d9a485796139db5aefa393892a0
mohsinehour <125808498+mohsinehour@users.noreply.github.com>
2024-03-13 14:34:48 -0700

    task15

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -14,5 +14,5 @@
       FROM tv_genres
 INNER JOIN tv_show_genres
         ON tv_genres.id = tv_show_genres.genre_id
-	  GROUP BY genre
-	  ORDER BY number_shows DESC;
+	GROUP BY genre
+	ORDER BY number_shows DESC;
