21df733bb59eff4e767d6e3f6eddf2f91a3fe075
Mohsine Hourmat Allah <125808498+mohsinehour@users.noreply.github.com>
2024-04-11 16:29:35 +0000

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script lists all states with a name starting with N
+This script lists all states
+with a name starting with N
 (upper N) from the database hbtn_0e_0_usa
 """
 
@@ -16,7 +17,9 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY 'N%' \
+                 ORDER BY states.id ASC")
     rows = cur.fetchall()
 
     for row in rows:
